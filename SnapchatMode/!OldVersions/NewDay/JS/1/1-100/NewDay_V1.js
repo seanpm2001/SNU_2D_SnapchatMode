cf2c9789c69f8173c63e7d99d2c66bc0be8542d6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-04 17:15:47 -0700

    NewDay_V1.js
    An archive of the first version of the New Day script file for this project.

---
+++ SnapchatMode/!OldVersions/NewDay/JS/1/1-100/NewDay_V1.js
@@ -0,0 +1,22 @@
+// Start of script
+/* NewDay.js
+Actions to perform on a new day
+License: GNU General Public License V3
+*/
+/* Info
+* Snapchat deletes user posts every day to provide a fresh new experience. This script will delete all submitted data from the previous day.
+Settings, profile picture, bio, etc. is not deleted
+*/
+// This script is not functional yet, and is merely just a pseudocode example. 
+function newDay() {
+ delete '$new&posts+past-24-hours';
+ break;
+}
+return newDay();
+break;
+/* File info
+* File type: JavaScript source file (*.js)
+* File version: 1 (2022, Wednesday, May 4th at 5:15 pm PST)
+* Line count (including blank lines and compiler line): 23
+*/
+// End of script
